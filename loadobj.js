f2dab6aae38bcdd9716f4fe9f014a9e4d1f6bab8
callmeliuchu <19931108github>
2022-04-30 10:13:27 +0800

    旋转

--- loadobj.js
+++ loadobj.js
@@ -4,6 +4,9 @@ class Loader{
         this.data = data;
         this.vertexs = [];
         this.faces= [];
+        this.max_position = [-1000,-1000,-1000]
+        this.min_position = [1000,1000,1000]
+        this.center = []
         this.init();
       }
     
@@ -13,6 +16,14 @@ class Loader{
                 let arr = [];
                 for(let num of s.substr(2,s.length-2).split(" ")){
                     arr.push(parseFloat(num));
+                }
+                for(let i=0;i<3;i++){
+                    if(this.max_position[i] < arr[i]){
+                        this.max_position[i] = arr[i];
+                    }
+                    if(this.min_position[i] > arr[i]){
+                        this.min_position[i] = arr[i];
+                    }
                 }
                 this.vertexs.push(arr);
             }
@@ -25,6 +36,9 @@ class Loader{
                 }
                 this.faces.push(arr);
             }
+        }
+        for(let i=0;i<3;i++){
+            this.center[i] = (this.max_position[i]+this.min_position[i])/2;
         }
     }
 
